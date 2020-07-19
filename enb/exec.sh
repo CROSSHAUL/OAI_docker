79fbd9008c4cdf1c4c86879dbef43374c50f4185
Julio Renner <julio.renner@sap.com>
2020-07-19 20:20:52 -0300

    improve enb build

--- enb/exec.sh
+++ enb/exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Initializing UEs ..."
-cd enb_folder/cmake_targets/ran_build/build/ && sudo -E ./lte-softmodem -O /enb_folder/ci-scripts/conf_files/rcc.band7.tm1.nfapi.conf > enb.log 2>&1
+cd enb/cmake_targets/ran_build/build/ && sudo -E ./lte-softmodem -O /enb/ci-scripts/conf_files/rcc.band7.tm1.nfapi.conf > enb.log 2>&1
