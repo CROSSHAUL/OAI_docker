380e2b685a50a6c8e133792eb58e217e34b24ae2
Julio Renner <julio.renner@sap.com>
2020-07-26 23:29:43 -0300

    fix rcc

--- rcc/exec.sh
+++ rcc/exec.sh
@@ -3,6 +3,6 @@
 sudo ln -sf /proc/1/fd/1 ./oai.txt
 
 sleep 5
-sudo -E ./targets/bin/lte-softmodem.Rel15 -O ./ci-scripts/conf_files/rcc.band7.tm1.nfapi.conf> ./oai.txt 2>&1 &
+sudo -E /root/enb/targets/bin/lte-softmodem.Rel15 -O /root/enb/ci-scripts/conf_files/rcc.band7.tm1.nfapi.conf
 
 exit 0
